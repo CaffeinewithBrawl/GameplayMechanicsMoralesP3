e972f8695e008221e667b0c48728ee04fe69dbc3
CaffeinewithBrawl <719048@student.dusd.net>
2025-09-29 11:58:08 -0700

    progress
    Check unity tutorial for where left off. Next lesson!

--- GameplayMechanicsP3Morales/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsP3Morales/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    private GameObject focalPoint; 
+    private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,5 +27,36 @@ public class PlayerController : MonoBehaviour
 
         // Makes player movement corrolate to the position of the camera (focal point). It always goes forward from camera.
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0); 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " +  collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
