68cd9e86aff18b0fcfa0947774d0b2de7be207e2
TheWaspsAreHere <719048@student.dusd.net>
2025-10-06 20:33:07 -0700

    Gave up
    My back hurts and the tutorial made a lot of grammar mistakes and coding mistakes.

--- GameplayMechanicsP3Morales/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsP3Morales/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,14 @@ public class PlayerController : MonoBehaviour
     private GameObject tmpRocket;
     private Coroutine powerupCountdown;
 
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+
+    bool smashing = false;
+    float floorY;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -66,6 +74,7 @@ public class PlayerController : MonoBehaviour
         currentPowerUp = PowerUpType.None;
         powerupIndicator.gameObject.SetActive(false);
     }
+
 
     private void OnCollisionEnter(Collision collision)
     {
